3098668c6e4b21070351099119d023c989be1c0e
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-04-06 19:42:52 -0700

    Add project files.

---
+++ AsyncInn/Data/AsyncdbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace AsyncInn.Data
+{
+    public class AsyncdbContext : DbContext
+    {
+        public AsyncdbContext(DbContextOptions<AsyncdbContext> options) : base(options)
+        {
+
+
+        }
+        //public DbSet<MODELNAME> TABLENAME { get; set; }
+
+    }
+}
